6b2264595682fae72ee83f626827cb07882fdbf2
csdnqiulian <csdnqiulian@126.com>
2017-03-10 15:59:07 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/test/test11111.java

--- src/test/test11111.java
+++ src/test/test11111.java
@@ -11,16 +11,6 @@ public class test11111 {
 		System.out.println("dd222222222222d");
 		System.out.println("xxxxxxxxxxxx");
 		System.out.println("dd222222222222d");
-		System.out.println("xxxxxxxxxxxxx23333333333333333333333xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-		System.out.println("111111111111111111111232323232323232323232323231111111111111111111111");\
-		System.out.println("ddd");
-		System.out.println("dd222222222222d");
-		System.out.println("xxxxxxxxxxxx");
-		System.out.println("dd222222222222d");
-		System.out.println("xxxxxxxxxxxxx23333333333333333333333xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-		System.out.println("111111111111111111111232323232323232323232323231111111111111111111111");
-		System.out.println("2222222222222222222222222222222222222222");
-		System.out.println("333333333333333333333333333333333333333 ");
 	}
 
 }
