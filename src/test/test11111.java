645acc784c22d760c242de1c4ae02a5f2aff4125
csdnqiulian <csdnqiulian@126.com>
2017-03-10 16:05:04 +0800

    sdf斯蒂芬

--- src/test/test11111.java
+++ src/test/test11111.java
@@ -7,11 +7,7 @@ public class test11111 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 		System.out.println("hello world zhangsan ");
-=======
-		System.out.println("hello world!!!");
->>>>>>> 1646908f0445b52b3b3ce245053d179327ced497
 	}
 
 }
